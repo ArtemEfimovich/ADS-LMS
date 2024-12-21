b779e1171f237df3f644a0ae1bfb5265c8c888a8
mac <artemfender@gmail.com>
2024-12-21 21:54:59 +0300

    lesson 07 done

--- src/by/it/group310951/a_efimovich/lesson07/C_EditDist.java
+++ src/by/it/group310951/a_efimovich/lesson07/C_EditDist.java
@@ -50,12 +50,46 @@ import java.util.Scanner;
 public class C_EditDist {
 
     String getDistanceEdinting(String one, String two) {
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        int n = one.length();
+        int m = two.length();
 
+        int[][] dp = new int[n + 1][m + 1];
 
-        String result = "";
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int cost = one.charAt(i - 1) == two.charAt(j - 1) ? 0 : 1;
+                dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + cost);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        int i = n, j = m;
+        while (i > 0 || j > 0) {
+            if (i > 0 && dp[i][j] == dp[i - 1][j] + 1) {
+                result.insert(0, "-" + one.charAt(i - 1) + ",");
+                i--;
+            } else if (j > 0 && dp[i][j] == dp[i][j - 1] + 1) {
+                result.insert(0, "+" + two.charAt(j - 1) + ",");
+                j--;
+            } else {
+                if (one.charAt(i - 1) == two.charAt(j - 1)) {
+                    result.insert(0, "#,");
+                } else {
+                    result.insert(0, "~" + two.charAt(j - 1) + ",");
+                }
+                i--;
+                j--;
+            }
+        }
+
+        return result.toString();
     }
 
 
@@ -64,9 +98,9 @@ public class C_EditDist {
         InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson07/dataABC.txt");
         C_EditDist instance = new C_EditDist();
         Scanner scanner = new Scanner(stream);
-        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
-        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
-        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
     }
 
 }
